6748e88290e8f76a741f4a544057ef79ea218e30
Shino-Thomas-dot <shinothomas17@gmail.com>
2024-09-28 14:53:24 +0530

    Database Connectivity and minor changes to form

--- Contact-SQL.php
+++ Contact-SQL.php
@@ -38,3 +38,46 @@ $psmt->close();
 $conn->close();
 ?>
 
+
+<?php
+use PHPMailer\PHPMailer\PHPMailer; //import kr raha hai
+use PHPMailer\PHPMailer\Exception; // along with exceptions
+
+require 'vendor/autoload.php'; // Autoload PHPMailer classes
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST['email'];   // Get the email from the form
+
+
+    $mail = new PHPMailer(true); // ek naya instance create kiya jo PHPmailer mein hai
+
+    try {
+        // Server settings
+        $mail->isSMTP();                                 // Set mailer to use SMTP
+        $mail->Host       = 'smtp.gmail.com';          // Specify main and backup SMTP servers
+        $mail->SMTPAuth   = true;                       // Enable SMTP authentication
+        $mail->Username   = 'shinothomas17@gmail.com';     // Your email address (Gmail)
+        $mail->Password   = 'rsdv oabi piby sngn';      // Your email password or App Password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Enable TLS encryption for secure tranmission of the mail
+        $mail->Port       = 587;                        //Port 25: Used for sending emails but often blocked by ISPs to reduce spam.
+        //Port 587: Recommended for email submission, supporting STARTTLS.
+        //Port 465: Used for SMTP over SSL (Secure Sockets Layer).
+        // Recipients
+        $mail->setFrom('shinothomas17@gmail.com', 'Shino Thomas'); // Sender's email and name
+        $mail->addAddress($email);                         // Add a recipient (the email entered by the user)
+
+        // Content
+        $mail->isHTML(true);                               // Set email format to HTML
+        $mail->Subject = 'Meeting Request Received -Shino Thomas';      // Subject of the email
+        $mail->Body    = 'We have received your request for a meeting. We will get back to you soon. Have a nice day!'; // Email body content
+
+        $mail->send();                                     // Send the email
+        echo 'Meeting request sent successfully!';        // Success message
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}"; // Error message if sending fails
+    }
+} else {
+    echo "Invalid request.";                             // Message for invalid request
+}
+?>
+
